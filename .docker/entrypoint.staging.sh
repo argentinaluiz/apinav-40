5197347c97596c51268da3d106a7510fa9bdedfd
Luiz Carlos <argentinaluiz@gmail.com>
2020-07-19 00:30:27 -0300

    Test #26

--- .docker/entrypoint.staging.sh
+++ .docker/entrypoint.staging.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+echo "3"
 if [ ! -f ".env" ]; then
+    echo "5"
   cp /var/www/.env.example /var/www/.env
 fi
 if [ ! -f ".env.testing" ]; then
+    echo "6"
   cp /var/www/.env.testing.example /var/www/.env.testing
 fi
-
+echo "10"
 php artisan create-databases
